ed6a4c06d6fa615592cad70c3b16325eeacc201f
thewitchcat <arfakhsyadz.work@outlook.com>
2026-03-12 11:54:29 +0700

    test(user-controller): add REST-assured integration tests for User endpoints

---
+++ src/test/java/com/thewitchcat/meetingroombooking/api/controller/UserControllerTest.java
@@ -0,0 +1,71 @@
+package com.thewitchcat.meetingroombooking.api.controller;
+
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.Test;
+
+import com.thewitchcat.meetingroombooking.api.dto.user.UserRequestDto;
+import com.thewitchcat.meetingroombooking.api.repository.UserRepository;
+
+import io.micronaut.test.extensions.junit5.annotation.MicronautTest;
+import io.restassured.http.ContentType;
+import io.restassured.specification.RequestSpecification;
+import jakarta.inject.Inject;
+
+import static org.hamcrest.Matchers.equalTo;
+
+@MicronautTest()
+public class UserControllerTest {
+
+  @Inject
+  UserRepository repository;
+  
+  @AfterEach
+  void cleanup() {
+    repository.deleteAll();
+  }
+  
+  @Test
+  public void testCreateUserEndpoint(RequestSpecification spec) {
+
+    UserRequestDto user = new UserRequestDto(
+      "Mike", 
+      "mike@example.com"
+    );
+    
+    spec
+      .contentType(ContentType.JSON)
+      .body(user)
+      .when()
+        .post("/users")
+      .then()
+        .statusCode(201)
+        .contentType("application/json")
+        .body("name", equalTo("Mike"))
+        .body("email", equalTo("mike@example.com"));
+  }
+  
+  @Test
+  public void testGetAllUsersEndpoint(RequestSpecification spec) {
+
+    UserRequestDto user = new UserRequestDto(
+      "Mike", 
+      "mike@example.com"
+    );
+    
+    spec
+      .contentType(ContentType.JSON)
+      .body(user)
+      .when()
+        .post("/users");
+    
+    spec
+      .when()
+        .get("/users")
+      .then()
+        .statusCode(200)
+        .contentType("application/json")
+        .body("size()", equalTo(1))
+        .body("name[0]", equalTo("Mike"))
+        .body("email[0]", equalTo("mike@example.com"));
+  }
+}
