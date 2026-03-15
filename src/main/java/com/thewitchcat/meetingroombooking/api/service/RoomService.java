7d62fa9d8accd4a9af0592a57ea484b29a133c1e
thewitchcat <arfakhsyadz.work@outlook.com>
2026-03-15 14:08:52 +0700

    fix(room): remove unnecessary import statement

--- src/main/java/com/thewitchcat/meetingroombooking/api/service/RoomService.java
+++ src/main/java/com/thewitchcat/meetingroombooking/api/service/RoomService.java
@@ -1,7 +1,6 @@
 package com.thewitchcat.meetingroombooking.api.service;
 
 import java.time.LocalDateTime;
-import java.util.List;
 import java.util.UUID;
 
 import com.thewitchcat.meetingroombooking.api.domain.Room;
